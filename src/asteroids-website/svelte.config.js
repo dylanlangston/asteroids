5fa9c5338d7b9ce9b1d94538330bd5e26428a56d
Dylan <dylanlangston@gmail.com>
2023-12-08 15:14:25 +0000

    Set Svelte fallback to index.html

--- src/asteroids-website/svelte.config.js
+++ src/asteroids-website/svelte.config.js
@@ -22,7 +22,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: 'index.html',
 			precompress: false,
 			strict: false
 		}),
