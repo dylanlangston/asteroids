2984efa9cccb5f7b132394602af76a3016774a38
Dylan <dylanlangston@gmail.com>
2023-12-08 15:58:32 +0000

    Fix Github 404

--- src/asteroids-website/svelte.config.js
+++ src/asteroids-website/svelte.config.js
@@ -22,7 +22,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html',
+			fallback: '404.html',
 			precompress: false,
 			strict: false
 		}),
