3930168ded27f407af74ec220e7414e873a3808f
Dylan Langston <dylanlangston@gmail.com>
2024-01-04 22:30:33 -0500

    Add game color palette

--- src/Colors.zig
+++ src/Colors.zig
@@ -1,6 +1,6 @@
 const raylib = @import("raylib");
 
-pub const Colors = struct {
+pub const DefaultColors = struct {
     pub const Transparent = raylib.Color.blank;
 
     pub const Tone = Color{
@@ -49,6 +49,29 @@ pub const Colors = struct {
         .Base = raylib.Color.brown,
         .Dark = raylib.Color.dark_brown,
         .Light = raylib.Color.beige,
+    };
+};
+
+pub const AKC12 = struct {
+    pub const Red = Color{
+        .Base = raylib.Color.init(194, 75, 110, 255),
+        .Dark = raylib.Color.init(167, 49, 105, 255),
+        .Light = raylib.Color.init(217, 98, 107, 255),
+    };
+    pub const Yellow = Color{
+        .Base = raylib.Color.init(255, 194, 122, 255),
+        .Dark = raylib.Color.init(236, 154, 109, 255),
+        .Light = raylib.Color.init(255, 235, 153, 255),
+    };
+    pub const Green = Color{
+        .Base = raylib.Color.init(106, 175, 157, 255),
+        .Dark = raylib.Color.init(53, 93, 104, 255),
+        .Light = raylib.Color.init(148, 197, 172, 255),
+    };
+    pub const Blue = Color{
+        .Base = raylib.Color.init(32, 20, 51, 255),
+        .Dark = raylib.Color.init(32, 17, 39, 255),
+        .Light = raylib.Color.init(27, 30, 52, 255),
     };
 };
 
